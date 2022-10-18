fd710e2e6d439a2f6b61b0e0ae8ef81bfba1948b
Oyewole Ayorinde <ayorindeoyewole@gmail.com>
2022-10-18 19:34:38 -0400

    modify app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String name = "oye";
+
+		system.out.printIn(name);
 	}
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mvn-nexus-sonar
+
+sample comment
